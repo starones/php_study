459c7107c5ca33d299c11fee8a9faf5d670a3aa3
starones <itstarone172@gmail.com>
2022-03-26 14:27:29 +0900

    lesson10

---
+++ lesson10.php
@@ -0,0 +1,18 @@
+<!--------- 関数 ---------->
+
+<?php
+
+// 変数の文字の数を表示（実行値は7になる）
+$str = 'progate';
+echo strlen($str);
+echo "<br>";
+
+// 配列の数を数える（実行値は3になる）
+$array = array('HTML', 'CSS', 'PHP');
+echo count($array);
+echo "<br>";
+
+// ランダムに数字を表示（実行値は10から18のどれか一つ）
+echo rand(10,18);
+
+?>
